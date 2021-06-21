a9432411945d5deaef9c56727cf2024002dcb351
Nemolite <g25092011@mail.ru>
2021-06-21 13:02:38 +0300

    start

---
+++ varimit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: Var imit
+* Plugin URI: https://github.com/Nemolite/varimit
+* Description: Имитация вариации.Формироует апселлы для каждой вариации 
+* Version: 1.0.0
+* Author: Nemolite
+* Author URI: http://vandraren.ru/
+* License: GPL2
+*/
+
+defined('ABSPATH') || exit;
+
+
+?>
