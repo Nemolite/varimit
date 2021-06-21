4c834ccfb529c9012903f8a08c5d3f17acdc4f07
Nemolite <g25092011@mail.ru>
2021-06-21 15:36:13 +0300

    Меню и подменю

--- varimit.php
+++ varimit.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Var imit
 * Plugin URI: https://github.com/Nemolite/varimit
-* Description: Имитация вариации.Формироует апселлы для каждой вариации 
+* Description: Имитация вариации.Формирует апселлы для каждой вариации 
 * Version: 1.0.0
 * Author: Nemolite
 * Author URI: http://vandraren.ru/
@@ -11,5 +11,63 @@
 
 defined('ABSPATH') || exit;
 
+/**
+ * Создаем пункт меню в админке "Вариации"
+ */
+
+add_action( 'admin_menu', 'register_menu_for_varimit' );
+
+function register_menu_for_varimit () {
+    
+    add_menu_page( 
+        'Вариации',       // Title
+        'Вариации ',      // Пункт меню
+        'manage_options', // Права доступа (кому будет видны пункты меню)
+        'varimit',        // slug
+        '', 
+        'dashicons-images-alt', // Иконка
+        30
+     );
+}
+
+/**
+ * Создаем подпункты главного меню вариации
+ */
+
+ /**
+ * Значения для вариаций
+ */ 
+
+add_action('admin_menu', 'register_menu_for_varimit_submenu_variation');
+
+function register_menu_for_varimit_submenu_variation() {
+    
+	add_submenu_page(
+		'varimit',                 // slug - главного меню
+		'Значения для вариаций',   // Title
+		'Значения для вариаций',   // Пункт подменю
+		'manage_options',          // Права доступа (кому будет видны пункты меню)
+		'variation',               // slug подменю
+		'varimit_submenu_variation',  // Название функции которая будет вызваться
+        5                          // Позиция в списке подменю  
+	);
+}
+
+ /**
+  * Список вариации
+  */
+  add_action('admin_menu', 'register_menu_for_varimit_submenu_variation_list');
+
+  function register_menu_for_varimit_submenu_variation_list() {
+      add_submenu_page(
+          'varimit',            // slug - главного меню
+          'Список вариаций',    // Title
+          'Список вариаций',    // Пункт подменю
+          'manage_options',     // Права доступа (кому будет видны пункты меню)
+          'variation-list',     // slug подменю
+          'varimit_submenu_variation_list',  // Название функции которая будет вызваться
+          10                    // Позиция в списке подменю  
+      );
+  }
 
 ?>
