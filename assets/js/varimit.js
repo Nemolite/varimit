e3f94fa7c134130a2ea5bd834a1913ba48c66a30
Nemolite <g25092011@mail.ru>
2021-07-01 07:17:05 +0300

    Вариации

---
+++ assets/js/varimit.js
@@ -0,0 +1,8 @@
+( function( $ ) {
+
+     
+    
+     const counter = $('.varimit-output-single-product-inner-left').attr('data-key_parent');
+console.log(counter);
+	
+} )( jQuery );
--- varimit.php
+++ varimit.php
@@ -12,70 +12,16 @@
 defined('ABSPATH') || exit;
 
 /**
- * Создаем пункт меню в админке "Вариации"
+ * Подключение скриптов и стилей
  */
 
-add_action( 'admin_menu', 'register_menu_for_varimit' );
-
-function register_menu_for_varimit () {
-    
-    add_menu_page( 
-        'Вариации',       // Title
-        'Вариации ',      // Пункт меню
-        'manage_options', // Права доступа (кому будет видны пункты меню)
-        'varimit',        // slug
-        '', 
-        'dashicons-images-alt', // Иконка
-        30
-     );
+function script_and_style_varimit(){
+  wp_enqueue_style( 'varimit-style',  plugins_url('assets/css/style.css', __FILE__));
+  wp_enqueue_script( 'varimit-script', plugins_url('assets/js/varimit.js', __FILE__),array(),'1.0.0','in_footer');
 }
+add_action( 'wp_enqueue_scripts', 'script_and_style_varimit' );
 
-/**
- * Создаем подпункты главного меню вариации
- */
 
- /**
- * Значения для вариаций
- */ 
-
-add_action('admin_menu', 'register_menu_for_varimit_submenu_variation');
-
-function register_menu_for_varimit_submenu_variation() {
-    
-	add_submenu_page(
-		'varimit',                 // slug - главного меню
-		'Значения для вариаций',   // Title
-		'Значения для вариаций',   // Пункт подменю
-		'manage_options',          // Права доступа (кому будет видны пункты меню)
-		'variation',               // slug подменю
-		'varimit_submenu_variation',  // Название функции которая будет вызваться
-        5                          // Позиция в списке подменю  
-	);
-}
-
- /**
-  * Список вариации
-  */
-  add_action('admin_menu', 'register_menu_for_varimit_submenu_variation_list');
-
-  function register_menu_for_varimit_submenu_variation_list() {
-      add_submenu_page(
-          'varimit',            // slug - главного меню
-          'Список вариаций',    // Title
-          'Список вариаций',    // Пункт подменю
-          'manage_options',     // Права доступа (кому будет видны пункты меню)
-          'variation-list',     // slug подменю
-          'varimit_submenu_variation_list',  // Название функции которая будет вызваться
-          10                    // Позиция в списке подменю  
-      );
-  }
-
-  /**
-   * Формируем Значения для вариации
-   */
-
-   function varimit_submenu_variation(){
-      
-   }
+require "inc/submenu.php";
 
 ?>
