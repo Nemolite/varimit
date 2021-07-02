f0ed3604fce0b5e05235b73aaf3ddfeddcf3a4f5
Nemolite <g25092011@mail.ru>
2021-07-02 18:16:16 +0300

    edit and delete

--- assets/js/varimit-admin.js
+++ assets/js/varimit-admin.js
@@ -1,26 +1,62 @@
 ( function( $ ) {
+     /**
+      * Добавление вариацию в таблицы базы данных
+      *
+      * @param   {[type]}  #submit-variation  [#submit-variation description]
+      * @param   {[type]}  click              [click description]
+      * @param   {[type]}  function           [function description]
+      *
+      * @return  {[type]}                     [return description]
+      */
     $('#submit-variation').on('click',function(){ 
-
-                            
+                        
          let variation_form = document.querySelector('#variation_form');
          let getDateForm = new FormData(variation_form);
          getDateForm.append("action", "add_new_variation"); 
-         // alert(myajax.ajaxurl);
+         
          $.ajax({
               url:myajax.ajaxurl, 
               data:getDateForm,
               processData : false,
               contentType : false,              
-              type:'POST',  
-              success:function(request){                                                            
-                   alert(request);            
-                                 
-              },
-              error: function (error) {
-                alert(error);
-            }
+              type:'POST', 
+              success:function(request){ 
+                   
+               setTimeout(function() { location.reload() }, 3000);
+               
+                                                          
+          },           
   
           });
      });
+
+/**
+ * Редактирование Вариации
+ *
+ * @param   {[type]}  #submit-variation-edit  [#submit-variation-edit description]
+ * @param   {[type]}  click                   [click description]
+ * @param   {[type]}  function                [function description]
+ *
+ * @return  {[type]}                          [return description]
+ */
+     $('#submit-variation-edit').on('click',function(){ 
+                        
+          let variation_form_edit = document.querySelector('#variation-form-edit');        
+          let getDateFormEdit = new FormData(variation_form_edit);
+          getDateFormEdit.append("action", "edit_variation");           
+          $.ajax({
+               url:myajax.ajaxurl, 
+               data:getDateFormEdit,
+               processData : false,
+               contentType : false,              
+               type:'POST',  
+               success:function(request){                                                            
+                    alert("Вариация успешно обновлена"); 
+                                                               
+               },
+              
+           });
+      });
+     
   
 } )( jQuery );
--- inc/database.php
+++ inc/database.php
@@ -9,7 +9,6 @@
   */
 
 add_action('wp_ajax_add_new_variation', 'varimit_add_new_variation'); 
-
 function varimit_add_new_variation(){ 
     global $wpdb;
 
@@ -42,5 +41,62 @@ function varimit_variation_display_from_db($results) {
     $results = $wpdb->get_results( 'SELECT * FROM '.$table_name, ARRAY_A );
     return $results;
 
+}
+
+/**
+ * Редактирование Вариации базы данных 
+ */
+add_action('wp_ajax_edit_variation', 'varimit_action_edit_variation'); 
+function varimit_action_edit_variation(){ 
+    global $wpdb;
+
+    if ( isset( $_POST['variation_label'] ) ) {
+        $variation_label = sanitize_text_field( $_POST['variation_label'] );
+    }
+
+    if ( isset( $_POST['variation_name'] ) ) {
+        $variation_name = sanitize_text_field( $_POST['variation_name'] );
+    }
+
+    if ( isset( $_POST['variation_id'] ) ) {
+        $variation_id = sanitize_text_field( $_POST['variation_id'] );
+    }
+
+    $table_name = $wpdb->prefix . 'varimit_variation';
+
+
+    $num_varimit = $wpdb->update( $table_name, [ 
+        'namevari' => $variation_label, 
+        'slugvari' => $variation_name
+    ],
+    [ 'id' => $variation_id ]
+ );
+       
+return $num_varimit;
+    wp_die();
+}
+
+/**
+ * Удаление Вариации базы данных 
+ */
+
+function varimit_delete_variation(){ 
+    global $wpdb;
+
+    $delete_id = isset( $_GET['delete'] ) ? absint( $_GET['delete'] ) : 0;
+
+    $table_name = $wpdb->prefix . 'varimit_variation';
+
+
+    $del_varimit = $wpdb->delete( $table_name,
+    [ 'id' => $delete_id ]
+ );
+ 
+ echo "Вариация удалена";
+
+header('Location: '. get_site_url() .'/wp-admin/edit.php?post_type=product&page=variation'); 
+
+return $del_varimit;
+    wp_die();
 }
  ?>
